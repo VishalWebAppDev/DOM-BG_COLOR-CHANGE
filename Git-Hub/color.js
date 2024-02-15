d4bf16af330562131131ede2d8155e49fbd252f1
VishalWebAppDev <158253625+VishalWebAppDev@users.noreply.github.com>
2024-02-15 15:56:01 +0530

    Add files via upload

---
+++ Git-Hub/color.js
@@ -0,0 +1,21 @@
+function backgroundColor1(){
+    document.body.style.backgroundColor = "red";
+    document.getElementById("Red").style.border="2px solid white";
+
+ } function backgroundColor2(){
+    document.body.style.backgroundColor = "Green";
+    document.getElementById("Green").style.border="2px solid white";
+
+ } function backgroundColor3(){
+    document.body.style.backgroundColor = "Yellow";
+    document.getElementById("Yellow").style.border="2px solid white";
+
+ } function backgroundColor4(){
+    document.body.style.backgroundColor = "pink";
+    document.getElementById("Pink").style.border="2px solid white";
+
+ } function backgroundColor5(){
+    document.body.style.backgroundColor = "Orange";
+    document.getElementById("Black").style.border="2px solid white";
+
+ }
